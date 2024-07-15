29c9a601bae745f4467cd26a79b450c8faee148a
Yash Aggarwal <yash.agg1818@gmail.com>
2024-07-15 17:46:43 +0530

    taking input in day1

---
+++ day1.java
@@ -0,0 +1,16 @@
+// taking input using javautilscanner and git commands
+import java.util.Scanner;
+public class day1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //input        \
+        System.out.println("Enter the first number:");
+        
+        int a= sc.nextInt();
+        System.out.println("Enter the second number:");
+        int b= sc.nextInt();
+        //output
+        System.out.println("The sum of the numbers is " +(a+b));
+
+    }
+}
