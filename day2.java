931330489acafddde1378aa1dca970e8d7e22916
Yash Aggarwal <yash.agg1818@gmail.com>
2024-07-15 17:48:38 +0530

    if else loop in day3 commit

---
+++ day2.java
@@ -0,0 +1,55 @@
+//if else statements for loop nested for loops and pattern printing 
+
+import java.util.Scanner;
+
+public class day2{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    // //if else for number divisible by 2  
+
+    // // //input 
+    // // System.out.println("Enter the number ");
+    // // int a= sc.nextInt();
+
+    // // if (a % 2 == 0) {
+    // //     System.out.println("The number is even");
+        
+    
+    // // } else {
+    // // System.out.println("The number is odd");
+    
+
+    // // }
+    
+    // // if else for sum of first natural numbers NOT CORRECT
+
+    // //input 
+    // int N=5;
+    // System.out.println("Enter the number");
+    // int i=sc.nextInt();
+    
+
+    // if (i<=N) {
+    //     int sum=0;
+    //     sum += i;
+    //     i++;
+        
+    // } 
+    // System.out.println("The sum of first 5 natural numbers are " +sum);
+
+    //loops
+    //print all natural numbers upto 10
+//     int i=1;
+//     while (i<=10) {
+//         System.out.println(i);
+//         i++;
+        
+//do while loop NOT CORRECT
+        // int i=11;
+        // do {
+        //     System.out.println(i);
+        // } while (i<=9); 
+        // System.out.println(i);
+    }
+ }
