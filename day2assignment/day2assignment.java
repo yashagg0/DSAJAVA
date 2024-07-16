121ff11ca92eedc69886a3e9da96ea423ad0c6ab
Yash Aggarwal <yash.agg1818@gmail.com>
2024-07-16 18:41:02 +0530

    assignment

---
+++ day2assignment/day2assignment.java
@@ -0,0 +1,15 @@
+//Q1
+public class day2assignment {
+    public static void main(String[] args) {
+        int N=5;
+        for (int i = 1; i <= N; i++) {
+            System.out.println(" " );
+            for (int j = 1; j <=N; j++) {
+                System.out.print("* ");
+                
+            }
+            
+        }
+    
+}
+}
