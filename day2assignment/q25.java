87715a68ae1172a401986d54a3988019a694d179
Yash Aggarwal <yash.agg1818@gmail.com>
2024-07-17 19:16:37 +0530

    assignment

---
+++ day2assignment/q25.java
@@ -0,0 +1,20 @@
+public class q25 {
+    public static void main(String[] args) {
+        int number =1;
+        int N=5;
+        for(int i=1; i<=N;i++){
+            for(int k=1; k<=N-i;k++){
+                System.out.print("    ");
+        }
+        for(int j=1; j<=2*i-1;j++){
+            if (number <= 25) {
+                System.out.print("  " +number);
+                number++;    
+            }
+            
+    
+}
+System.out.println();
+}
+}
+}
---
+++ day2assignment/q26.java
@@ -0,0 +1,15 @@
+public class q26 {
+    public static void main(String[] args) {
+        int N=5;
+        for(int i=1; i<=N;i++){
+            for(int k=1; k<=N-i;k++){
+                System.out.print("  ");
+        }
+        for(int j=1; j<=2*i-1;j++){
+            System.out.print(" "+j);
+    
+}
+System.out.println();
+}
+}
+}
