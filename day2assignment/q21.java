ba02e4d44e256a3ab82d8873f7ac3b4b949bba6e
Yash Aggarwal <yash.agg1818@gmail.com>
2024-07-19 17:45:52 +0530

    not solved yet

--- day2assignment/q21.java
+++ day2assignment/q21.java
@@ -5,9 +5,18 @@ public class q21 {
             for (int j=1;j<=i;j++) {
                 System.out.print("* ");
             
-        }   
+        }
         System.out.println();
-    
-}
+    }  
+        for (int i=1;i<=N;i++) {
+            for (int k=6;k<=(2*N-i);k++) {
+                System.out.print("  ");
+            for (int j=6;j<=2*i;j++) {
+                System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
+
