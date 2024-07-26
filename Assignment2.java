95e8e9b622718e81ef2ba3768d0cb0c037555626
Yash Aggarwal <yash.agg1818@gmail.com>
2024-07-26 22:27:30 +0530

    Assignment 2 questinos

---
+++ Assignment2.java
@@ -0,0 +1,76 @@
+public class Assignment2 {
+
+// celcius to faranhite
+public static void main(String args[]) {
+    // Your Code Here
+//     Scanner sc = new Scanner(System.in);
+    
+//     double c;
+
+//     int minF = 0;
+//     int maxF = 100;
+//     int step = 20;
+
+//     for (int f = minF; f <= maxF; f += step){
+//         c= (5.0/9.0) * (f - 32) ;
+//         System.out.println(f+ "\t" +c);
+//     } 
+
+// }
+//}
+
+
+// reverse a number 
+
+// int number = 123456789, reverse = 0;
+//         while (number != 0){
+//             int remainder = number % 10;
+//             reverse = reverse*10 + remainder;   
+//             number = number / 10;
+//         }
+//         System.out.println(reverse);
+//     }
+// }    
+
+
+// gcd of two numbers
+
+// int x = 12, y = 8, gcd = 1;    
+// for(int i = 1; i <= x && i <= y; i++)  
+// {  
+// if(x%i==0 && y%i==0)  
+// gcd = i;  
+// }  
+// System.out.printf("GCD of 12 and 8 is: %d", x, y, gcd);  
+// }  
+// }  
+
+//lcm of two numbers
+
+int x = 4, y = 6, gcd = 1;    
+for(int i = 1; i <= x && i <= y; i++)  
+{  
+if(x%i==0 && y%i==0)  
+gcd = i;  
+}  
+int lcm = (x*y)/gcd;
+
+System.out.println(lcm);
+}  
+}  
+
+
+// fibonacci series upto 10
+
+    // Your Code Here
+//     int n1=0, n2=1,n3,i, count=10;
+//     System.out.print(n1+ " " +n2);
+//     for (i=2; i<count; i++){
+//         n3= n1+n2;
+//         System.out.print(" " +n3);
+//         n1=n2;
+//         n2=n3;
+//     }
+// }
+// }
+
