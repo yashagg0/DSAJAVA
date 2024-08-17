e79f829c0703cd032e64c0b7cad1cc61cbfc3b8a
Yash Aggarwal <yash.agg1818@gmail.com>
2024-08-17 12:24:23 +0530

    day 11

---
+++ day11.java
@@ -0,0 +1,31 @@
+public class day11{   // DAY 11 3 QUESTIONS ON LEETCODE
+    //NOW THIS IS CLASS 12 SUFFIX AND PREFIX SUM AND STRINGS
+    public static void main(String[] args) {
+        // String s = "hello";
+        // String s2 = new String("hellow");
+        // System.out.println("s2 " +s2);
+        // String s3 = s;
+        // System.out.println("s3 " +s3);
+        // String s4 = new String(s3);
+        // System.out.println("s4 " +s4);
+        // char[] c = {'h','e','l','l','o'};
+        // System.out.println(s.length());
+        // System.out.println("s " +s);
+        // System.out.println(c);
+        // String s5 = new String(c);
+        // System.out.println("s5 " +s5);
+
+    
+        // System.out.println(s.substring(2,4));
+
+        // String s1 = "Racecar";
+        // String s2 = new String("RaCecar");
+        // System.out.println(s1.equals(s2));
+        // System.out.println(s1.equalsIgnoreCase(s2));
+
+        String s1 = "R";
+        String s2 = new String("A");
+        System.out.println(s1.compareTo(s2));
+        
+    }
+}
